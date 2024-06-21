22658f5dfaf41d5b40b86c7d342f8d9851d6e7ef
Liloupar <hunji66@gmail.com>
2024-06-21 13:09:44 +0800

    faq build

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  base: '/docs/',
   title: '清墨',
   description: '清墨 - 纯净文本阅读器',
   icon: '/rspress-icon.png',
